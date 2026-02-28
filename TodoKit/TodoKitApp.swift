9efc42c665511be021433f2ba02b41877416da0e
laisongxian <172588067@qq.com>
2026-02-28 16:34:19 +0800

    Initial Commit

---
+++ TodoKit/TodoKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoKitApp.swift
+//  TodoKit
+//
+//  Created by laisx on 2026/2/28.
+//
+
+import SwiftUI
+
+@main
+struct TodoKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
